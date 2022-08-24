4d495048c969aa8cad13d60827a57fa2ca7bacf1
GeetStudAnshu <109529851+GeetStudAnshu@users.noreply.github.com>
2022-08-24 20:00:33 +0530

    Add files via upload

---
+++ J2EE/Servlet1/src/java/NewServlet.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class NewServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            out.println("<h1>Hello World</h1>");
+        }
+    }
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
